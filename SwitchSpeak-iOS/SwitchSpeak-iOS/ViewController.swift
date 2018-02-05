5ec7ff18751014456915ad365187a2ffae636fed
Robert Gerdisch <rgerdisch@gmail.com>
2018-02-05 12:52:57 -0500

    Added SwitchSpeak iOS project

---
+++ SwitchSpeak-iOS/SwitchSpeak-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchSpeak-iOS
+//
+//  Created by Robert Gerdisch on 2/5/18.
+//  Copyright © 2018 SwitchSpeak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
