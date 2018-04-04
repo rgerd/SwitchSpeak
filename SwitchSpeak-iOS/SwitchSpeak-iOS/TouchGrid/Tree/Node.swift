218c88f6dcc71f83533f49d79aea11156bf1c01e
Robert Gerdisch <rgerdisch@gmail.com>
2018-04-04 15:18:12 -0400

    Polished mergeNode

--- SwitchSpeak-iOS/SwitchSpeak-iOS/TouchGrid/Tree/Node.swift
+++ SwitchSpeak-iOS/SwitchSpeak-iOS/TouchGrid/Tree/Node.swift
@@ -13,8 +13,8 @@ import UIKit
 a Node would always be a non-leaf node in the 'Tree' data structure
 */
 class Node {
-	var parentNode: Node?			//	the parent node could be nil
-	var childNodes: [Node] = []		//	initialised with an empty child list
+	var parentNode:Node?			//	the parent node could be nil
+	var childNodes:[Node] = []		//	initialised with an empty child list
 	var dummy:Bool = false
 
 	init() {
@@ -52,21 +52,20 @@ class Node {
 
     // If this node only has one non-dummy child node, that node is returned by this function; otherwise the function returns the node itself
     func mergeNode() -> Node {
-        var nonDummyChildCount = 0
-        var nonDummyChildren : Node? = nil
-
+        var nonDummyChild:Node? = nil
+        
         for childNode in self.childNodes {
-            if childNode.dummy == false {
-                nonDummyChildCount = nonDummyChildCount + 1
-                nonDummyChildren = childNode
+            if !childNode.dummy {
+                if nonDummyChild == nil {
+                    nonDummyChild = childNode
+                } else {
+                    return self;
+                }
             }
         }
-        if nonDummyChildCount == 1 {
-            return nonDummyChildren!
-        }
-        else{
-            return self
-        }
+        
+        // We might still have never encountered a non-dummy child
+        return nonDummyChild == nil ? self : nonDummyChild!
     }
     
     // it will make every child node shrink if that child has only single nonDummy child node
